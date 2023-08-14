7c6561ff480532584c36fb904e2b7f994899667e
Gustavo Alves <unidragorn@gmail.com>
2023-08-14 19:24:26 -0300

    requisito 2

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,34 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
 
+  private int numero;
+  private int votos;
+
+  public PessoaCandidata(String nome, int numero) {
+    super(nome);
+
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return this.numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return this.votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
